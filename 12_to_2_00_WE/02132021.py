db0b6c8c57e6e9f32ad079cb0c58cc9cd54b3fda
Ishan Suryan <ishansuryan@gmail.com>
2021-02-13 13:31:45 +0530

    02132021

---
+++ 12_to_2_00_WE/02132021.py
@@ -0,0 +1,13 @@
+print("Hello World",'How are you',)
+print()
+print(3+0)
+
+a = 10
+b = 20
+print(a+b)
+
+print('He said that "he will cme today"')
+print("he's gonnam come today")
+print('''He sais that "he's gonna be there."''')
+print("He sais that \"he's gonna be there.\"")
+print(r'c:\new folder')
