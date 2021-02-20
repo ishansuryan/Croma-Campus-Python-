9cbf45ccd5f853d7815ce7eebe6cc9d5b456a9b5
ishansuryan <ishansuryan@gmail.com>
2021-02-20 16:56:25 +0530

    02202021

---
+++ TEMP/WE_01_30_02_30_02202021/WE_01_30_02_30_02202021.py
@@ -0,0 +1,25 @@
+# print(sin(30))
+
+import math
+print(math.sin(30))
+print(math.factorial(6))
+
+import math as m
+print(m.sin(30))
+
+from math import cos,sin,sqrt
+print(sqrt(30))
+
+import random
+a = random.randint(1000,9999)
+print(a)
+
+c = random.randrange(1,30)
+print(c)
+
+l= [1,3,2,5,9,7,2]
+print(random.choice(l))
+random.shuffle(l)
+print(random.randrange(1000,10000),random.choice(l),sep=',')
+# print(l)
+
