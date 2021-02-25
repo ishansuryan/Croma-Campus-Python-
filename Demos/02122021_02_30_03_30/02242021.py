bad46e23c6310343356e1bccda7cba6e21a2b9c7
ishansuryan <ishansuryan@gmail.com>
2021-02-25 11:01:14 +0530

    02252021_09:30-11:00 AM

---
+++ Demos/02122021_02_30_03_30/02242021.py
@@ -0,0 +1,11 @@
+# a = input("Enter Number1: ")
+# b = input('Enter Number2: ')
+# print("The sum is: ", int(a)+int(b))
+
+import random
+otp = random.randint(100000,999999)
+print(otp)
+
+import calendar
+print(calendar.calendar(2050))
+
