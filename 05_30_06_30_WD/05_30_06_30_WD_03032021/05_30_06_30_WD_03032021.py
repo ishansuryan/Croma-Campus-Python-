5843c7a2cb868c4d7738a96c0159da5d2f89e904
ishansuryan <ishansuryan@gmail.com>
2021-03-03 18:28:09 +0530

    05:30 to 06:30 PM 03032021

---
+++ 05_30_06_30_WD/05_30_06_30_WD_03032021/05_30_06_30_WD_03032021.py
@@ -0,0 +1,22 @@
+# print("Hello world", end ='\n')
+# print("How are you?")  
+
+print("Hello world","How are you?", sep=' ')
+''' string, int, float, boolean, comlex, None'''
+a = 1000
+print(type(a), id(a))
+
+a = 0.2
+print(type(a),id(a))
+
+a = 'True'
+print(type(a),id(a))
+
+a = 0.3 +2j         # complex(.3,2)
+print(type(a),id(a))
+
+a = True
+print(type(a),id(a))
+
+a = None
+print(type(a),id(a))
