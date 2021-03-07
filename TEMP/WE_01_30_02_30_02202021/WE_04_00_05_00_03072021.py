039d4874502dd954ef0148a3013c832c60a0ba5c
ishansuryan <ishansuryan@gmail.com>
2021-03-07 18:12:19 +0530

    05:00PM to 06:00 Pm 03072021

---
+++ TEMP/WE_01_30_02_30_02202021/WE_04_00_05_00_03072021.py
@@ -0,0 +1,13 @@
+print('Hello',"World",sep = ' ',end ='->>>>>>>')
+print('How are you?')
+print("he's there")
+print('''he said that "he's gonna be there."''')
+print('he said that "he\'s gonna be there."')
+print(r"C:\newfolder\Deep Learning Specialization with TF2 - CloudxLab _ CloudxLab.html")
+
+s = 'Hello World how ae you'
+print(s[4])
+print(s[-1])
+print(s[6:11])
+print(s[0:5:2])
+print(s[-1:-5:-1])
