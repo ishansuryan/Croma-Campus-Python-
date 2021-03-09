72eb52b5492046d40087a37325234c34374e9feb
ishansuryan <ishansuryan@gmail.com>
2021-03-09 16:04:23 +0530

    02:30PM to 04:00 PM 03092021

--- 07_Feb_2021.py
+++ 07_Feb_2021.py
@@ -1,2 +1,11 @@
-print('hello world','How are you','there')
-print(r'''hello guys what's going in there, shaym said that "he will come" and c:\new folder''')
+# print('hello world','How are you','there')
+# print('''hello guys what's going in there, shaym said that "he will come" and c:\new folder''')
+print("hello, how it's going")
+print('he siad the "he is going to come there"')
+print('''he siad the "he's going to come there"''')
+print('he siad the "he\'s going to come there"')
+print("c:\\new folder")
+print(r"c:\new folder")
+
+s = 'the quick brown fox jumpes over the lazy dog'
+print(s[::-1])
