f7af16755697ab7f9711e8d98239612041dd5958
ishansuryan <ishansuryan@gmail.com>
2021-03-11 18:38:20 +0530

    05:30 to 06:30 Pm 03112021

---
+++ 05_30_06_30_WD/05_30_06_30_WD_03112021/05_30_06_30_WD_03112021.py
@@ -0,0 +1,55 @@
+# a = 10
+# print(type(a))
+# # a = '10'
+# # print(type(a))
+
+# b = 5
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(int(a/b))
+
+# c = '10'
+# print(type(c))
+# c = int(c)
+# print(type(c),c)
+
+# print(a%b)
+# print(a//b)
+
+# print('-'*5)
+# s = 'hello'
+# s1 = 'world'
+# s2 =s + s1
+# print(s + ' ' +s1)
+
+
+
+
+# print(10/2*3)
+# print(10/3*2)
+
+
+
+# a = input("Enter a number:")
+# b = int(input("Enter a number:"))
+
+a = 2
+b = 4
+
+if type(a) == str:
+    print('number must be intiger type')
+
+elif type(a) == float:
+    print('a is float type')
+
+elif type(a) == complex:
+    print('a is complex number')
+
+else:
+    print(a+b)
+    
+print('hello')
+
+    
