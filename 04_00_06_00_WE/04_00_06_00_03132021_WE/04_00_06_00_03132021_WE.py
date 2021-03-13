a83d168c0626f47ad075b7da08e481fc6d934400
ishansuryan <ishansuryan@gmail.com>
2021-03-13 18:00:36 +0530

    02:00 to 04:00 PM 03132021

---
+++ 04_00_06_00_WE/04_00_06_00_03132021_WE/04_00_06_00_03132021_WE.py
@@ -0,0 +1,34 @@
+print('hello world',end = '------>')
+print("How are you?")
+
+print('hello','how are you?',sep = '>>>----++++')
+
+a = 10
+print('line no 7:',type(a))
+
+a = 10.20
+print('line no 10:', type(a))
+
+a = '10'
+print('line no 13:', type(a))
+
+a = True
+print('line no 16:', type(a))
+
+a = complex(2)
+print('line no 19:', type(a), a)
+
+a = None
+print('line no 22:', type(a))
+
+#    01234
+s = 'Hello'
+print(s[4])
+
+
+
+print("he's coming")
+print('He said that "he is coming')
+print('''He said that "He's coming''')
+print('he\'s coming')
+print('c:\\new folder')
