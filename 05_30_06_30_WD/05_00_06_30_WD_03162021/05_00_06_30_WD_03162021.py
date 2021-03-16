5c326bebfda5399864fe6db70084c17e04609782
ishansuryan <ishansuryan@gmail.com>
2021-03-16 17:34:36 +0530

    03:30 to 05:00 PM 03162021

---
+++ 05_30_06_30_WD/05_00_06_30_WD_03162021/05_00_06_30_WD_03162021.py
@@ -0,0 +1,33 @@
+# (0°C × 9/5) + 32 = 32°F
+c = 37
+f = (c* (9/5)) + 32
+print(f)
+
+print(True and False)
+print(True and True)
+print(False and True)
+print( False and False)
+
+print(True   or False)
+print(True   or True)
+print(False  or True)
+print( False or False)
+
+print(not(True   or False))
+print(not(True   or True))
+print(not(False  or True))
+print(not(False  or False))
+
+s= 'hi'
+print('line no. 22','H' not in s)
+
+a = 10
+print( 10 is 10)
+print( a == 10)
+
+a = -0.1
+if  a:
+    print("Non zero number")
+
+else:
+    print('zero number')
