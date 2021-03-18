4b2e5a2a46ef8358e856260ef2b8b8a72dc3b0b5
ishansuryan <ishansuryan@gmail.com>
2021-03-18 17:11:33 +0530

    03:30PM to 05:00 PM 03182021

--- 04_00_05_30_PM_WD/03_30_05_00_WD_03182021/03_30_05_00_WD_03182021.py
+++ 04_00_05_30_PM_WD/03_30_05_00_WD_03182021/03_30_05_00_WD_03182021.py
@@ -4,18 +4,32 @@
 
 # x = devide(10,0)
 
-def dec_devide(func):
-    def inner(a,b):
-        if b == 0:
-            print('devision by zero is not defined')
-            return 
-        return func(a,b)
-    return inner
+# def dec_devide(func):
+#     def inner(a,b):
+#         if b == 0:
+#             print('devision by zero is not defined')
+#             return 
+#         return func(a,b)
+#     return inner
 
-@dec_devide
-def devide(a,b):
-    return a/b
+# @dec_devide
+# def devide(a,b):
+#     return a/b
 
 
 # devide = dec_devide(devide)     # devide = inner
-x = devide(10,0)
+# x = devide(10,0)
+
+# arguments and keyword arguments
+
+def add(*args, **kwargs):
+    print(type(args))
+    s = 0
+    for i in args:
+        s +=i
+    print(type(kwargs))
+    print('  ',kwargs['a'])
+    return s
+
+print(add(1,2,3,4,5, a = 10,b =1,ccc=20))
+# print(add(10,20, bvbgbgb=45))
