d41ec7ca59e4f0b126f866a44dccadd372dcd732
ishansuryan <ishansuryan@gmail.com>
2021-03-20 18:08:34 +0530

    04:00 PM to 06:00 Pm 03202021

---
+++ 04_00_06_00_WE/04_00_06_00_WE_03202021/04_00_06_00_WE_03202021.py
@@ -0,0 +1,25 @@
+print("Hello world",)
+print("How are you?")
+
+print('hi', 'where have you been?',"hey there",'kkkkkksep',sep = '----->',end = ' Testing')
+print(" wewfwf ")
+print('10')
+
+s = 'The quick brown fox jumpes over the lazy dog'
+#    012345678
+print(type(s))
+print(s[4])
+print(s[-1])
+print(len(s))
+print(s[43])
+
+print(s[4:9])
+print(s[4:9:2])
+print(s[4:5:1])
+print(s[:9])
+print(s[43:40:-1])
+print(s[::-1])
+
+
+
+print('''he said that "he's going to come"''')
