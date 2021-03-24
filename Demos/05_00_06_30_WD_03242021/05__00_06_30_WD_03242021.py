dfbfb2b4b8516b866df896d96335b7186573ac21
ishansuryan <ishansuryan@gmail.com>
2021-03-24 12:25:10 +0530

    09:30 AM to 11:00 AM 03242021

---
+++ Demos/05_00_06_30_WD_03242021/05__00_06_30_WD_03242021.py
@@ -0,0 +1,23 @@
+print('Hello World')
+print("How are you?",'Are you there?', sep = '<><><>')
+
+import random
+otp = random.randint(100000,999999)
+print(otp)
+a = 'Student'
+print('Line No. 8:',type(a))
+
+a = 10
+print('Line No. 11:',type(a))
+
+a = 10.25
+print('Line No. 14:',type(a))
+
+a = True
+print('Line No. 17:',type(a))
+
+a =complex(3,5)
+print('Line No. 20:',type(a))
+
+a = None
+print('Line No. 23:',type(a))
