93ef370d49793e8ce0603bf79ad8c022e7502415
ishansuryan <ishansuryan@gmail.com>
2021-03-28 11:05:06 +0530

    09:00 am to 11:00 am 03282021

---
+++ 09_00_11_00_WE/09_00_11_00_WE_03282021/09_00_11_00_WE_03282021.py
@@ -0,0 +1,24 @@
+source = r"E:\folder"
+
+import sys
+
+# sys.path.append(source)
+
+# from player_demo import player
+
+# player1 = player('Croma')
+# # player1.get_name()
+
+# a = True
+
+# print(sys.getsizeof(a))
+# import os
+# print(os.getcwd())
+
+# while True:
+#     x = input()
+#     if x.lower() == 'q':
+#             print('aborting')
+#             os.abort()
+
+print((sys.argv))
