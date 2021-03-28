2be051c2f53d93a6e184a5fa8ff6452bd5414363
ishansuryan <ishansuryan@gmail.com>
2021-03-28 11:05:48 +0530

    03282021

---
+++ 04_00_06_00_WE/04_00_06_00_WE_03272021/04_00_06_00_WE_03272021.py
@@ -0,0 +1,22 @@
+s = 'The quick brown fox jumpes over the lazy dog'
+#    0123456789
+# print(s[2])
+# print(s[4:9])
+# print(s[::-1])
+
+
+# s1 = 'helLo HOw aRe YOU?'
+# print('Line no:9', s1.capitalize())
+# print('Line no:10',s1.casefold())
+# print('Line no:11',s1.lower())
+# print('Line no:12',s1.center(30))
+# s1 = 'hi'
+# print(s1.center(4))
+# print('hi')
+
+# print(s.count("E".lower()))
+# print(s.endswith('y dog'))
+# print(s.find('e',3,25))
+
+for i in range(1,11):
+    print('the number is {:<2} the sqauare is {:>3} the cube is {:^4}'.format(i,i**2,i**3))
