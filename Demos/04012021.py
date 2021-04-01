5e9dbe6c60e047c2af13b8f2b49262fa89752b94
ishansuryan <ishansuryan@gmail.com>
2021-04-01 15:46:15 +0530

    02:30 to 04:00 PM 04012021

---
+++ Demos/04012021.py
@@ -0,0 +1,6 @@
+# a = int(input('Enter the number 1: '))
+# b = input('Enter The number 2: ')
+# print('The sum is',a+int(b))
+import random
+otp = random.randint(100000,999999)
+print('Otp is:',otp)
