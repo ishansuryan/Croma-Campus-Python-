ca01710283a54cccffdd5cab30ed783bc647f9c0
ishansuryan <ishansuryan@gmail.com>
2021-04-03 17:47:59 +0530

    04:00 to 06:00 Pm 04032021

---
+++ Demos/04032021.py
@@ -0,0 +1,11 @@
+standard_input = [6,5]
+a = int(input("Enter number1: "))
+b = input('Enter number2: ')
+print('the sum is:',a+int(b),sep ='->',end = '-----')
+print('Hello World')
+
+
+
+
+import random
+print(random.randint(10000,99999))
