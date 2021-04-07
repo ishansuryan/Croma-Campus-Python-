548fc9d5f1cf8498da65e9e195837343a15277c4
ishansuryan <ishansuryan@gmail.com>
2021-04-07 17:33:33 +0530

    04:30 to 05:30 PM 04072021

---
+++ 04_30_05_30_WD/04_30_05_30_WD_04072021/04_30_05_30_WD_04072021.py
@@ -0,0 +1,23 @@
+s = r'c:\new folder\table'
+#     0123456789
+# print(s)
+
+# path = r"E:\Croma-Campus-Python-\Data\DL\2. Training Deep Neural Networks.pdf"
+
+# print(path)
+
+
+print(s[0:17:3])     # [start value : end value : step size]
+
+'''
+Indexing rules:
+
+1) Stat value is always grater than end value
+
+'''
+
+print(len(s))
+
+print(s[0:19])
+
+print(s[::-1])
