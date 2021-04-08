1f26b73c4aa2e3255bb76aa18d179d31eff832a2
ishansuryan <ishansuryan@gmail.com>
2021-04-08 18:43:46 +0530

    04082021

---
+++ 04_30_05_30_WD/04_30_05_30_WD_04082021/04_30_05_30_WD_04082021.py
@@ -0,0 +1,20 @@
+# s = 40
+# t = 20
+# v = s/t
+# print(type(v))
+# print(v)
+
+# s = 'hi wldfwhoroege htej h python'
+# #    012345678
+# print('line no 8:',type(s))
+
+# print(s[3])
+# print(s[-1])
+# print(s[0:12:2])            # slicing
+# print('line no 14:',s[::-1])            # reversing ideom
+
+# print(help(str))
+
+s = 'hi How Are You?'
+print(s.capitalize())
+print(s.lower().count('h'))
---
+++ 05_30_06_30_WD/05_30_06_30_WD_04082021/05_30_06_30_WD_04082021.py
@@ -0,0 +1,32 @@
+# a = 10
+# b = 10
+
+# if ~ a & b:
+#     print('Not empty')
+# else: 
+#     print ('empty')
+    
+# from math import sin,pi, tan
+
+# print(tan((90*pi)/180))
+
+# if pi < 22/7:
+#     print('Error')
+# else:
+#     print("Running Successfully")
+    
+# print(pi)
+# print(22/7)
+
+# import calendar
+# print(calendar.calendar(2021)) 
+
+import random
+otp = random.randint(10000000,99999999)
+print(otp) 
+
+x = random.randrange(1000,9999)
+print(x)
+
+
+
