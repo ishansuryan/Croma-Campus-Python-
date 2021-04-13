f2616ac1885eae1f97ece3bd2fed4e6c049701cb
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-04-13 19:47:49 +0530

    05_30_06_30_04132021

---
+++ 05_30_06_30_WD/05_30_06_30_04132021/05_30_06_30_04132021.py
@@ -0,0 +1,26 @@
+# from datetime import datetime
+# str = '04-April-2021'
+
+# s = datetime.strptime(str,'%d-%B-%Y')
+# print(s)
+# print(type(s))
+
+import re
+
+'''
+
+hi
+123-456-7890
+(902)-(449)-(4802)in
+1234567890
+
+croma@.com
+croma@campus.com
+croma_batch@campus.com
+email.id@email.gov.in
+email@email.org
+
+nothing is here
+we are using rregular expressions today
+
+'''
