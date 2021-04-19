b04d2981b3c6358b0d39abd83ef9d7c1b6255259
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-04-19 18:59:18 +0530

    11:00 to 12:30 Pm 04192021

---
+++ 04_30_05_30_WD/11_00_12_30_04192021/11_00_12_30_04192021.py
@@ -0,0 +1,9 @@
+print("Hello world",end = '')
+print('How are you?')
+
+print('Hi there','How aits going on there','I was waiting for this time.',sep = '---<>')
+
+a = int(input('Enter number 1: '))
+b = input('Enter number 2: ')
+
+print('The sum is :', a+int(b))
