2115508734401c28057300d0f3aa73f7ac79f403
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-04-22 12:26:56 +0530

    11:00 to 12:30 PM 04222021

---
+++ 04_30_05_30_WD/11_00_12_30_04222021/11_00_12_30_04222021.py
@@ -0,0 +1,20 @@
+a = 10 
+print('line no.02', type(a) )
+
+a = '10' 
+print('line no.05', type(a) )
+
+a = 10.0 
+print('line no.08', type(a) )
+
+a = True 
+print('line no.11', type(a) )
+
+a = 2+3j        # complex(2,3) 
+print('line no.14', type(a) )
+
+a = None 
+print('line no.17', type(a) )
+
+
+# Conditional Execution of statement
