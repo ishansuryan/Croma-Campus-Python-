3c7fa0abaecd9c4bc001329fe18641db65dbacae
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-04-27 20:11:28 +0530

    05:30 to 06:30 Pm 04272021

---
+++ WD_Batch_01/05_30_06_30_04272021/05_30_06_30_04272021.py
@@ -0,0 +1,15 @@
+print("Hello world")
+
+s = 'Hello World'
+#    0123456789
+# string Indexing
+
+print(s[1])
+print(s[1:4])
+print(s[6:10])
+print(s[5:3:1])         # Wrong limits hence no output 
+print(s[-5])
+print(s[-5:])
+print(s[:-6:-1])
+print(s[:5])
+print(s[::-1])
