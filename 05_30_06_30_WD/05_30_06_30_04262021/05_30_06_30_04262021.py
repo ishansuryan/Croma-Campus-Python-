837328e99b27024f00a51fc80e5f9c0894f89321
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-04-28 16:48:23 +0530

    04262021

---
+++ 05_30_06_30_WD/05_30_06_30_04262021/05_30_06_30_04262021.py
@@ -0,0 +1,8 @@
+# print('Hello World',end ='--->')
+# print('How are you?')                   # \n
+
+print('Hello','World',10,6,'There',sep ='\n')
+
+number1 = int(input("Enter Number 1: "))
+number2 = input('Enter Number 2: ')
+print('the sum is', number1+ int(number2))
