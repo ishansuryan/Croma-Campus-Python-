4ab08fd2fef6834eab876a1c443eddb57faae4b8
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-05-10 14:52:39 +0530

    11:00 to 12:30 PM 05102021

---
+++ 04_30_05_30_WD/11_00_12_30_00_05102021/11_00_12_30_00_05102021.py
@@ -0,0 +1,18 @@
+l = (1,2,3,4,5,6)
+
+l2 = ('1',[2,3],{},(3,))
+
+print(type(l))
+
+b = (1,)
+print(type(b))
+print(len(b))
+
+l1 = 'hello'
+v = tuple(l1)
+print(type(v))
+
+print(help(tuple))
+
+
+# print(l2>l)
