f38c0f7670573e1038c0f0251e86b3bb84bd9ef2
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-05-11 18:46:56 +0530

    05:30 to 06:30 PM 05112021

---
+++ WD_Batch_01/05_30_06_30_05112021/05_30_06_30_05112021.py
@@ -0,0 +1,12 @@
+print('Hello World',end = '->>>>>')
+print('How are you?',2000,'3','hi there',sep = '------>')
+
+num1 = int(input("Enter Number 1: "))
+num2 = input('Enter Number 2: ')
+print('type of num1',type(num1))
+print('type of num2',type(num2))
+print('The Sum of above numbers is:',num1+int(num2))
+print(type(num2))
+
+num1 = 1.2
+num1 = 'Milk Shake'
