88b67f26e8b05c95333b40d283c7346cabb84f93
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-05-13 18:58:03 +0530

    05:30 to 06:30 Pm 05132021

---
+++ WD_Batch_01/05_30_06_30_05132021/05_30_06_30_05132021.py
@@ -0,0 +1,22 @@
+s = 'hello How Are YOu'
+print('line no.2:->', s.capitalize())
+
+print('Line no.4:->', s.casefold())
+
+print('Line No.6:->',s.center(30,'>'))
+
+print('Line No.8:->',s.count('o'))
+
+print('Line No.10:->',s.endswith('u?'))
+
+print('line No.12:->',s.find('oo'))
+
+print('line No.14:->',s.index('o'))
+t = 'HelloHowareYou12354465464'
+print('Line No.16:->',t.isalnum())
+
+print('Line No.18:->','class'.isidentifier())
+
+print('Line No. 20:->','Hello \n how re you'.isprintable())
+
+print('Line No.22:->',s.title())
