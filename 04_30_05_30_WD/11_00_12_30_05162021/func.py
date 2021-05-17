c092bb80fd5dd4abc96237556d8eec1709a01bf2
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-05-17 13:31:10 +0530

    11:00 to 12:30 Pm 05172021

---
+++ 04_30_05_30_WD/11_00_12_30_05162021/func.py
@@ -0,0 +1,20 @@
+# def addition():
+#     a = int(input('Enter a number'))
+#     b = int(input('Enter a number'))
+#     print('The sum of two numbers is:', a+b)
+
+# standard_input = ['10','-5','6','100']
+
+# addition()
+# # print('first call')
+
+# addition()
+# # print('second call')
+
+def series_generator():
+    for x in range(10):
+        print(x)
+# print('func executed_)(*&^%$#@!')
+# print('The name of main function in func file:',__name__)     # __main__
+# if __name__ == '__main__':
+#     series_generator()
---
+++ 04_30_05_30_WD/11_00_12_30_05162021/test.py
@@ -0,0 +1,5 @@
+import os
+from func import series_generator
+# os.path.curdir()
+print('-'*50)
+series_generator()
