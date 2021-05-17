e7a4ea4b59f4611b11f03ed40b0135a1ab1d644c
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-05-17 18:48:40 +0530

    05:00 to 06:30 Pm 05172021

---
+++ WD_Batch_01/05_00_06_30_05172021/05_00_06_30_05172021.py
@@ -0,0 +1,14 @@
+l = ['hi','how', 'are', 'you']
+print('->'.join(l),end = '.\n')
+
+print('hi there'.ljust(20,'-'))
+
+print('   hi how are you'.lstrip())
+print('   hi    '.rstrip())
+print('   hi    '.strip())
+
+print('   hi    '.removeprefix(' '))
+
+print('hi how are you'.partition(' '))
+
+print('hi how are you'.split())
