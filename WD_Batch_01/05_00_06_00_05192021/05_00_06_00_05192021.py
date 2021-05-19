b8a485e12c80749016261aa134cd2aabd367fe68
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-05-19 19:47:37 +0530

    05:00 to 06:30 PM 05192021

---
+++ WD_Batch_01/05_00_06_00_05192021/05_00_06_00_05192021.py
@@ -0,0 +1,23 @@
+'''This is a string which needs to be reversed.
+The quick brown fox jumps overthe lazy dog'''
+
+# He said that "He's goin tocome today"
+
+print('''He said that "He's goin tocome today"''')
+
+
+'''c:\newfolder\tableb\books'''
+
+print(help(str))
+
+y = [4,5,1]
+y.sort()
+print(y)
+
+a=['he','she','we'] 
+a.sort()
+print(a)
+
+pi = 22/7
+
+print(f'{pi:32.30f}')
