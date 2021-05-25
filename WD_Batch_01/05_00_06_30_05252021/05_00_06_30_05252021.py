5db0a780d953ffd3d5dc547180361bdf45941ccc
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-05-25 18:33:24 +0530

    05:00 PM TO 06:30 PM 05252021

---
+++ WD_Batch_01/05_00_06_30_05252021/05_00_06_30_05252021.py
@@ -0,0 +1,62 @@
+# Write a program to check between gartest of 3 numbers
+
+a = 10
+b = 100
+c = 100
+# if a==b==c:
+#     print("All are equal")
+# elif a>=b:
+#     if a == b:
+
+#         if b>c:
+#             print('a & b are equal and grater than c')
+#         else:
+#             print('c is gratest')
+    
+#     elif a>=c:
+
+#         if a==c :
+#             print('a and c are equal and grater than b')   
+#         else :
+#             print('a is gratest')
+    
+#     else:
+#         print('c is gratest')
+    
+
+# elif b>=c:
+    
+#     if b ==c:
+#         print(' b and c are equal and grater than a')
+#     elif b>c:
+#         print('b is gratest')
+
+# else: 
+#     print('c is gratest')
+
+# and operator
+c =1
+b = "1"
+# if a>b and a>c:
+#     print('a is gratest')
+
+# if a>b or b>c:
+#     print('Or cindition executed')
+
+s = 'vowels'
+
+if 'o' in s:
+    print('O is inside S')
+
+if b is c:
+    print('is statement executed')
+
+if c==b:
+    print('comparison executed')
+
+if 'z' not in s:
+    print('"not in" executed')
+
+if a is not c:
+    print('"is not" executed')
+
