510c6178755c96123bdf24623d689545a5b3c537
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-05-29 15:51:12 +0530

    09:00 AM TO 11:00 MA 05292021

---
+++ WE_Batch_01/09_00_11_00_05292021/09_00_11_00_05292021.py
@@ -0,0 +1,32 @@
+# cout<<""
+# print()
+
+# cin>>
+# input()
+# writing a program for adding 2 numbers in c++
+
+'''
+# include<iostream.h>
+#include<conio.h>
+void main(){
+    clrscr()
+    int a,b,c;
+    a = cin>>:
+    b = cin>>;
+    c = a+b
+    cout<<'The Sum is"<<c
+}
+'''
+# for python 
+
+# a = int(input("Enter a number"))     # standard in python input always returns string
+# b = input("Enter a number")
+# print('The sum is',a+int(b))
+# c =input("Please input a number")
+
+#print statement
+
+print('hello world') 
+
+print('How are you?')
+
