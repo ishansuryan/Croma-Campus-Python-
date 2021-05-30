e5220b83c0edbc220be43abbe3c7ad7462ad8bbf
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-05-30 11:13:30 +0530

    09:00 AM TO 11:00 AM 05302021

---
+++ WE_Batch_01/09_00_10_00_05302021/09_00_10_00_05302021.py
@@ -0,0 +1,44 @@
+# print('Hello world',end = '-->')        #\n:-> a new line character in computer
+# print('How are you?')
+
+# write a program to give following outputs using multiple statements
+
+# string_1 = 'Hello'
+# string_2 = 'How are You?'
+# string_3 = 'What are yoy doing?'
+
+# Output = 'Hello How are you?--->What are you doing?'
+
+# print('Hello','How are you?',10,2,0,5,0,3,0,sep = '---->')
+# 'Hello<><><><>How are you?<><><><>What are you doing?'
+
+# Data Types 
+a = 1 
+print('Line no. 17',type(a))
+
+a = 1.5
+print('Line no. 20',type(a))
+
+a = '1' 
+print('Line no. 23',type(a))
+
+a = complex(1) 
+print('Line no. 17',type(a),a)
+
+a = True
+print('Line no. 17',type(a))
+
+a = None
+print('Line no. 17',type(a))
+
+s = 'Hello how are you?'
+#    0123456789
+print(s[17])
+
+print(s[-4])        # negative indexing
+
+print(s[1],s[2],s[3],sep='')
+
+print(s[1:4])
+
+print(s[0:29:2])
