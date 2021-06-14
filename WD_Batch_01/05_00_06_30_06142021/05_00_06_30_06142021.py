8a839f0967518b7875fc7a4bab4a269987ad6927
Ishan Suryan <77906805+ishansuryan@users.noreply.github.com>
2021-06-14 22:49:44 +0530

    05:00 PM TO 06:30 PM 06142021

---
+++ WD_Batch_01/05_00_06_30_06142021/05_00_06_30_06142021.py
@@ -0,0 +1,28 @@
+# modules
+
+# import time 
+# print(time.time())
+# t = time.strftime('%A %d->%b-%Y   %z')
+# start = time.time()
+# time.sleep(1)
+# print(t)
+# print( 'time taken is', time.time()-start)
+
+# print(time.strftime('%c'))
+# print(help(time.strftime))
+
+from datetime import datetime
+
+x = datetime.now()
+# print(x)
+
+''' 
+Wap to print current date time in given format 
+Name of weekday Month-Day-Year time in 12 hr format
+'''
+import random
+random.seed(0)
+otp = random.randint(100000,999999)
+print(otp)
+otp = random.randrange(1000,10000)
+print(otp)
